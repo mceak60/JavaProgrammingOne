62c48d248df4f95d5b8f37fcddbbf0ea59d432ea
Nick Gonzalez <ng0973693@RK03-B1225-02.francistuttle.edu>
2018-01-11 13:52:18 -0600

    Added Chapter 5 attendance

--- src/Chapter5/C5_46.java
+++ src/Chapter5/C5_46.java
@@ -1,9 +1,64 @@
 package Chapter5;
 
+import java.util.Scanner;
+
 /**
+ * Program to play Rock Paper Scissors
  *
  * @author Nick Gonzalez
  */
 public class C5_46 {
 
+    /**
+     * Main Method
+     *
+     * @param args arguments from command line prompt
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int ComputerGuess, PlayerGuess;
+        int CompCount = 0;
+        int PlayerCount = 0;
+        do {
+            System.out.println("Enter 0 for rock, 1 for paper, or 2 for scissors");
+            PlayerGuess = input.nextInt();
+            ComputerGuess = (int) (Math.random() * 3);
+
+            switch (PlayerGuess) {
+                case 0:
+                    if (ComputerGuess == 0) {
+                        System.out.println("It's a tie");
+                    } else if (ComputerGuess == 1) {
+                        System.out.println("You lose");
+                        CompCount++;
+                    } else if (ComputerGuess == 2) {
+                        System.out.println("You win");
+                        PlayerCount++;
+                    }
+                    break;
+                case 1:
+                    if (ComputerGuess == 0) {
+                        System.out.println("You win");
+                        PlayerCount++;
+                    } else if (ComputerGuess == 1) {
+                        System.out.println("It's a tie");
+                    } else if (ComputerGuess == 2) {
+                        System.out.println("You lose");
+                        CompCount++;
+                    }
+                    break;
+                case 2:
+                    if (ComputerGuess == 0) {
+                        System.out.println("You lose");
+                        CompCount++;
+                    } else if (ComputerGuess == 1) {
+                        System.out.println("You win");
+                        PlayerCount++;
+                    } else if (ComputerGuess == 2) {
+                        System.out.println("It's a tie");
+                    }
+                    break;
+            }
+        } while (PlayerCount < 2 && CompCount < 2);
+    }
 }
